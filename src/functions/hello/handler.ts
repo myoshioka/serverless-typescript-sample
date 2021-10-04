38c65612cded0e512749fa5782ff42b0bdbfdff1
myoshioka <y.mako2@gmail.com>
2021-10-04 15:10:39 +0900

    Initial commit

---
+++ src/functions/hello/handler.ts
@@ -0,0 +1,20 @@
+import 'source-map-support/register'
+
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway'
+import { formatJSONResponse } from '@libs/apiGateway'
+import { middyfy } from '@libs/lambda'
+
+import schema from './schema'
+
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
+  event,
+) => {
+  console.log(event.body)
+  console.log(event.queryStringParameters)
+  return formatJSONResponse({
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    // event,
+  })
+}
+
+export const main = middyfy(hello)
